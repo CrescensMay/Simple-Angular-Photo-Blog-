ed01dd42b8f10b0c802604b88b774cf719af00de
Fidele <crescens.kob@outlook.fr>
2017-09-27 02:11:49 +0800

    used structural directives, added buttons like and show comments/hiding comments

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -9,5 +9,10 @@ export class EntryComponent{
     title: string = 'My first Photo';
     photo: string = 'http://placehold.it/800x500?text=Angular Basics';
     description: string = 'A Description of my first Photo';
+    comments: any[] = [
+        { name: "Crescens", comment: "Belle tof" },
+        { name: "May", comment: "Top tof" },
+        { name: "Fidele", comment: "J'aime bien la tof" }
+    ]
 
 }
