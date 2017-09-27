6b8eff5249bbe5485491b752fd9e13fffdccb78e
Fidele <crescens.kob@outlook.fr>
2017-09-27 13:47:55 +0800

    creating a service

---
+++ src/app/entries/shared/entry.model.ts
@@ -0,0 +1,8 @@
+export class Entry{
+    title: string;
+    // photo: string = 'http://placehold.it/800x500?text=Angular Basics';
+    photo: string;
+    description: string;
+    comments: any[];
+
+}
